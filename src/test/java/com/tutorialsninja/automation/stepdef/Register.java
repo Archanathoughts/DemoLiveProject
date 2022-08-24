5a3d23a74e20536ef68682ff63d6c28651bb60a0
Archana <a.rchana0486@gmail.com>
2022-08-24 17:29:03 +0530

    completed automating all the scenarios in the register feature file

--- src/test/java/com/tutorialsninja/automation/stepdef/Register.java
+++ src/test/java/com/tutorialsninja/automation/stepdef/Register.java
@@ -1,35 +1,95 @@
 package com.tutorialsninja.automation.stepdef;
 
+import java.util.Map;
+
+import org.junit.Assert;
+
 import com.tutorialsninja.automation.base.Base;
+import com.tutorialsninja.automation.framework.Elements;
+import com.tutorialsninja.automation.pages.AccountSuccessPage;
+import com.tutorialsninja.automation.pages.HeaderSection;
+import com.tutorialsninja.automation.pages.RegisterPage;
 
 import cucumber.api.DataTable;
 import cucumber.api.PendingException;
-import cucumber.api.java.en.*
-;
+import cucumber.api.java.en.*;
+
 
 public class Register {
-	@Given("^I navigate to Account Registration page$")
+	HeaderSection headerSection=new HeaderSection();
+	RegisterPage registerPage=new RegisterPage();
+	AccountSuccessPage accountSuccessPage=new AccountSuccessPage();
+
+@Given("^I launch the application$")
+public void i_launch_the_application()  {
+	Base.driver.get(Base.reader.getUrl());
+    }
+
+
+	
+	@And("^I navigate to Account Registration page$")
 	public void i_navigate_to_Account_Registration_page()  {
-		Base.driver.get(Base.reader.getUrl());	    
+		Elements.click(HeaderSection.myAccountlink);
+	    Elements.click(HeaderSection.register);
 	}
 
 	@When("^I provide all the below valid details$")
 	public void i_provide_all_the_below_valid_details(DataTable dataTable)  {
-	    //
+	    RegisterPage.enterAllDetails(dataTable,"unique");
 	}
 
+	
 	@When("^I select the Privacy Policy$")
 	public void i_select_the_Privacy_Policy() {
+		Elements.click(RegisterPage.privacyPolicy);
 	   	}
 
 	@And("^I click on Continue Button$")
 	public void i_click_on_Continue_Button(){
+		Elements.click(RegisterPage.continueButton);
 	   
 	}
 
 	@And("^I should see that the user account has successfully created$")
-	public void i_should_see_that_the_user_account_has_successfully_created() throws Throwable {
-	   	}
+	public void i_should_see_that_the_user_account_has_successfully_created() {
+		Assert.assertTrue(Elements.isDisplayed(AccountSuccessPage.successBreadcrumb));
+		
+	}
+	
+	@Then("^I should see that the User Account is not created$")
+	public void i_should_see_that_the_User_Account_is_not_created()  {
+	   Assert.assertTrue(Elements.isDisplayed(RegisterPage.registerBreadcrumb));
+	}
+
+	@And("^I should see the error messages informing the user to fill the mandatory fields$")
+	public void i_should_see_the_error_messages_informing_the_user_to_fill_the_mandatory_fields()  {
+	   Assert.assertTrue(Elements.isDisplayed(RegisterPage.firstNameWarning)); 
+	   Assert.assertTrue(Elements.isDisplayed(RegisterPage.lastNameWarning));
+	   Assert.assertTrue(Elements.isDisplayed(RegisterPage.emailWarning));
+	   Assert.assertTrue(Elements.isDisplayed(RegisterPage.telephoneWarning));
+	   Assert.assertTrue(Elements.isDisplayed(RegisterPage.passwordWarning));
+	   Assert.assertTrue(Elements.isDisplayed(RegisterPage.mainWarning));
+	   
+	}
+	
+	@And("^I subscribe to Newsletter$")
+	public void i_subscribe_to_Newsletter()  {
+		Elements.click(RegisterPage.yesToNewsletter);
+	    	}
+	
+
+@When("^I provide the below duplicate details into the fields$")
+public void i_provide_the_below_duplicate_details_into_the_fields(DataTable arg1) {
+	RegisterPage.enterAllDetails(dataTable,"duplicate");
+    
+}
+
+@Then("^I should see the warning message stating that the user is already created$")
+public void i_should_see_the_warning_message_stating_that_the_user_is_already_created()  {
+   
+}
+
+
 
 
 
--- src/test/resources/FeatureFiles/Register.feature
+++ src/test/resources/FeatureFiles/Register.feature
@@ -1,4 +1,3 @@
-
 Feature: Registration functionality scenario.
 @Register @One
 Scenario: Verify whether the user is able to register into the application by providing all the details
@@ -8,10 +7,47 @@ And I navigate to Account Registration page
 When I provide all the below valid details
 |FirstName |Ravi             |
 |LastName  | Kiran            |
-| Email     | ravi.kiran@gmail.com |
+| Email     | ravi.kiran1@gmail.com |
 |Telephone |9954223123        |
 | Password  |rkiran           |
 And I select the Privacy Policy
 And I click on Continue Button
 Then I should see that the user account has successfully created
 
+@Register @Two
+ Scenario: Verify whether the user is not allowed to register on skipping mandatory fields
+  	Given I launch the application
+  	And I navigate to Account Registration page
+  	When I click on Continue Button
+  	Then I should see that the User Account is not created
+  	And I should see the error messages informing the user to fill the mandatory fields
+
+
+@Register @Three
+  Scenario: Verify whether the user is able to register into the application by opting for Newsletter subscription
+  	Given I launch the application
+  	And I navigate to Account Registration page
+  	When I provide all the below valid details
+    	| FirstName | Ravi 									|
+    	| LastName  | Kiran 								|
+    	| Email		| Ravi.kiran2@gmail.com 				|
+    	| Telephone	| 9212345678							|
+    	| Password	| rkiran								|
+    And I subscribe to Newsletter
+    And I select the Privacy Policy
+    And I click on Continue Button
+    Then I should see that the user account has successfully created
+    
+    @Register @Four
+    Scenario: Verify whether the user is restricted from registring a duplicate account
+  	Given I launch the application
+  	And I navigate to Account Registration page
+  	When I provide the below duplicate details into the fields
+  		| FirstName | Ravi 									|
+    	| LastName  | Kiran 								|
+    	| Email		| Ravi.kiran1@gmail.com 				|
+    	| Telephone	| 9212345678							|
+    	| Password	| rkiran								|
+    And I select the Privacy Policy
+    And I click on Continue Button
+	Then I should see the warning message stating that the user is already created
